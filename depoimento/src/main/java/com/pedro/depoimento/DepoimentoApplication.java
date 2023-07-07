054bafa07392dc45c5ee69fd52a4da57a4e0a4bb
Joao Pedro <joao.araujo@pagcerto.com.br>
2023-07-06 21:23:42 -0300

    Commit inicial

---
+++ depoimento/src/main/java/com/pedro/depoimento/DepoimentoApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.depoimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepoimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepoimentoApplication.class, args);
+	}
+
+}
---
+++ depoimento/src/test/java/com/pedro/depoimento/DepoimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.depoimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepoimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
